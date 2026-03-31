ff8721727e3c44403a07f20599f872517021fcea
vircalimlim <killerfinisher12@gmail.com>
2026-03-31 23:42:15 +0800

    migration > audit log.

---
+++ database/migrations/2026_03_31_141051_create_audit_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->enum('event_type', ['create_ip', 'update_label', 'delete_ip', 'login', 'logout']);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('ip_address_id')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audit_logs');
+    }
+};
