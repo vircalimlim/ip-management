1b2dea0e6cf06b9a7c72d676ffbb1125a8b3ded3
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2026-03-31 18:15:51 +0800

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('role', ['superadmin', 'admin'])->default('admin');
             $table->rememberToken();
             $table->timestamps();
         });
